8f402f2486540c88c56bb374746f6cf9c4480f34
Maneesha <maneesha@Maneeshas-MacBook-Pro.local>
2013-05-23 10:40:29 -0500

    My first commit

---
+++ GitMobilemakersHack/GitMobilemakersHack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitMobilemakersHack
+//
+//  Created by Maneesha on 5/23/13.
+//  Copyright (c) 2013 Maneesha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitMobilemakersHack/GitMobilemakersHack/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitMobilemakersHack
+//
+//  Created by Maneesha on 5/23/13.
+//  Copyright (c) 2013 Maneesha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
