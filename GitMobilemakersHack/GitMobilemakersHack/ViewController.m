acc725a1e4741cfa541f484f8c6fc0ff0610b8f2
ymaneesha <ymaneesha87@gmail.com>
2013-05-23 11:30:03 -0500

    Update ViewController.m

--- GitMobilemakersHack/GitMobilemakersHack/ViewController.m
+++ GitMobilemakersHack/GitMobilemakersHack/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"Anything here");
+    NSLog(@"Please don't do this");
 }
 
 - (void)didReceiveMemoryWarning
